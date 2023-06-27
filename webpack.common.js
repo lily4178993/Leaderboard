2d614144ae6139a0fb719d421e052835f2f65125
lily4178993 <nellytelli@gmail.com>
2023-06-27 19:57:26 +0000

    Install style-loader and css-loader.

--- webpack.common.js
+++ webpack.common.js
@@ -8,4 +8,12 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
