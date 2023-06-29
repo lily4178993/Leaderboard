f474618d1c0cbc4cba2831e0d383b67afffd3764
lily4178993 <nellytelli@gmail.com>
2023-06-29 11:40:15 +0000

    Add the correct mandatory parameters for POST action

--- src/utils/api/create.js
+++ src/utils/api/create.js
@@ -1,12 +1,12 @@
 const CREATE_NEW_PLAYER = async (gameID, playerName, currentScore) => {
   const REQUEST_URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`;
   const newObject = {
-    player: playerName,
-    score: `${currentScore}`,
+    user: playerName,
+    score: currentScore,
   };
   const REQUEST = new Request(REQUEST_URL, {
     method: 'POST',
-    header: { 'Content-type': 'application/json' },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(newObject),
   });
   await fetch(REQUEST);
