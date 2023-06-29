1af89a6aaffd765df098fa62047fefc293ef5d12
lily4178993 <nellytelli@gmail.com>
2023-06-29 11:42:39 +0000

    Add additional verification for an empty string.

--- src/utils/inputFieldValidity.js
+++ src/utils/inputFieldValidity.js
@@ -1,14 +1,15 @@
+import myGameID from './api/gameID.js';
 import createNewPlayer from './api/create.js';
 
 const CHECK_INPUT_VALIDITY = () => {
   const INPUT_USERNAME = document.getElementById('username');
   const INPUT_SCORE = document.getElementById('score');
 
-  const INPUT_USERNAME_VALUE = INPUT_USERNAME.value.trim();
-  const INPUT_SCORE_VALUE = INPUT_SCORE.value.trim();
+  const INPUT_USERNAME_VALUE = INPUT_USERNAME.value;
+  const INPUT_SCORE_VALUE = parseInt(INPUT_SCORE.value, 10);
 
-  if (INPUT_SCORE_VALUE && INPUT_USERNAME_VALUE) {
-    createNewPlayer('Zl4d7I488d4eb0267g2fUdz', INPUT_USERNAME_VALUE, INPUT_SCORE_VALUE);
+  if (INPUT_SCORE_VALUE && INPUT_USERNAME_VALUE && INPUT_USERNAME_VALUE.trim() !== '') {
+    createNewPlayer(`${myGameID}`, INPUT_USERNAME_VALUE, INPUT_SCORE_VALUE);
   }
   INPUT_USERNAME.value = '';
   INPUT_SCORE.value = '';
