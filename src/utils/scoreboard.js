1f58b2a6d8bddcb037eef7e8cea1bb38db667d82
lily4178993 <nellytelli@gmail.com>
2023-06-29 07:20:24 +0000

    Add eventListerner to 'Form' and refresh button

--- src/utils/scoreboard.js
+++ src/utils/scoreboard.js
@@ -1,14 +1,15 @@
+import getAllPlayers from './api/read.js';
+
 const SCORE_BOARD = document.querySelector('.scoreboard-container');
 const SCORE_LIST = document.querySelector('.scoreboard-container .body');
 
-const MY_SCOREBOARD = () => {
-  SCORE_LIST.innerHTML = `
-  <li>Name 400</li>
-  <li>Name 233</li>
-  <li>Name 209</li>
-  <li>Name 29</li>
-`;
+const MY_BOARD_LIST = () => {
+  getAllPlayers.forEach((getPlayer) => {
+    const LIST_ITEM = document.createElement('li');
+    LIST_ITEM.innerHTML = `${getPlayer.player} ${getPlayer.score}`;
+    SCORE_LIST.appendChild(LIST_ITEM);
+  });
   SCORE_BOARD.appendChild(SCORE_LIST);
 };
 
-module.exports = MY_SCOREBOARD;
+export { MY_BOARD_LIST as default };
